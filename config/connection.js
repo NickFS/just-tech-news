4f4e719988e49108778c65b222f9b5c27750044d
NickFS <nickolas.t.foster@gmail.com>
2020-10-24 01:27:32 -0600

    post model creation

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+// impoirt the Sequelize constructor form the library
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+// create conneciton to our database, pass in your Mysql information for username and password.
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+ host: 'localhost',
+ dialect: 'mysql',
+ port: 3306,
+});
+
+
+module.exports = sequelize;
---
+++ models/index.js
@@ -0,0 +1,5 @@
+const User = require('./User');
+const Post = require('./Post');
+
+module.export = { User, Post };
+module.exports = { User };

5c07ff65086b0ac54127ed708d204e4786cc9899
NickFS <nickolas.t.foster@gmail.com>
2020-10-30 00:00:27 -0600

    created initial handlebar homepage

--- config/connection.js
+++ config/connection.js
@@ -6,9 +6,9 @@ require('dotenv').config();
 // create conneciton to our database, pass in your Mysql information for username and password.
 let sequelize;
 
-if(process.env.JAWSDB_URL) sequelize = new Seqeulize(process.env.JAWSDB_URL);
+if(process.env.JAWSDB_URL) sequelize = new Sequelize(process.env.JAWSDB_URL);
 else {
-    sequelize = new Seqeulize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
         dialect: 'mysql',
         port: 3306
